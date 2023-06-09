695c52d9b2d1c0017567e87310358b545f6e8b20
ChocoLord <raph.street@yandex.ru>
2023-06-09 18:46:00 +0400

    Modify config

--- stage3/tools/model_tools.py
+++ stage3/tools/model_tools.py
@@ -24,14 +24,25 @@ name2model_class = {
 
 def cfg2model(cfg):
     """ Returns model according to it config """
-    model = name2model_class[cfg["name"]](
-            encoder_name=cfg["encoder_name"],
-            encoder_weights=cfg["encoder_weights"],
-            in_channels=3,
-            classes=1,
-            activation="sigmoid",
-    ).to(cfg["device"])
+    if cfg["name"] == "fpn":
+        model = name2model_class[cfg["name"]](
+                encoder_name=cfg["encoder_name"],
+                encoder_weights=cfg["encoder_weights"],
+                in_channels=3,
+                classes=1,
+                activation="sigmoid",
+                decoder_merge_policy=cfg["decoder_merge_policy"]
+        ).to(cfg["device"])
     
+    else:
+        model = name2model_class[cfg["name"]](
+                encoder_name=cfg["encoder_name"],
+                encoder_weights=cfg["encoder_weights"],
+                in_channels=3,
+                classes=1,
+                activation="sigmoid",
+        ).to(cfg["device"])
+        
     
     if cfg["load_pretrained"]:
         model.load_state_dict(torch.load(os.path.join(cfg["pretrained_path"], "model.pt")))
